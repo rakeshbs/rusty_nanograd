0d78ea1bb94f16af3070d161b201c3de53ef83e2
rakeshbs <rakeshbs@gmail.com>
2023-10-31 23:00:44 +0530

    Added optimizer

---
+++ src/optimizer.rs
@@ -0,0 +1,27 @@
+use crate::node::*;
+
+pub struct Optimizer {
+    parameters: Vec<NodeRef>,
+    learning_rate: f32,
+    batch_size: usize,
+}
+
+impl Optimizer {
+    pub fn new(parameters: Vec<NodeRef>, learning_rate: f32, batch_size: usize) -> Optimizer {
+        Optimizer {
+            parameters,
+            learning_rate,
+            batch_size,
+        }
+    }
+    pub fn step(&self) {
+        for param in &self.parameters {
+            param.step(self.learning_rate, self.batch_size);
+        }
+    }
+    pub fn zero_grad(&self) {
+        for param in &self.parameters {
+            param.zero_grad();
+        }
+    }
+}
