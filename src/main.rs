fbf080cf70d50c869995e8b14a37daf649b3725a
rakeshbs <rakeshbs@gmail.com>
2023-10-31 23:01:16 +0530

    Added test graph and network

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,91 @@
+mod nn;
 mod node;
+mod optimizer;
+mod sine_generator;
+use crate::nn::*;
 use crate::node::*;
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::rc::Rc;
+use crate::optimizer::*;
 
-fn main() {
-    let a = val(1.);
-    let b = val(2.);
-    let c = mul(&a, &a);
-    let d = mul(&b, &b);
-    let e = add(&c, &d);
+fn main() -> () {
+    test_computational_graph();
+    test_neural_network();
+}
+
+struct Network {
+    l1: LinearLayer,
+    l2: LinearLayer,
+}
+
+impl Network {
+    pub fn new() -> Network {
+        let l1 = LinearLayer::new(4, 10);
+        let l2 = LinearLayer::new(10, 3);
+        Network { l1, l2 }
+    }
+
+    pub fn forward(&self, inputs: &Vec<NodeRef>) -> Vec<NodeRef> {
+        let mut x = self.l1.forward(&inputs);
+        x = x.iter().map(|x| relu(&x)).collect();
+        x = self.l2.forward(&x);
+        x
+    }
+
+    pub fn parameters(&self) -> Vec<NodeRef> {
+        let mut params = Vec::new();
+        params.append(&mut self.l1.parameters());
+        params.append(&mut self.l2.parameters());
+        return params;
+    }
+}
+
+fn test_neural_network() {
+    let nn = Network::new();
+    let x = array_to_val(vec![1., 2., 3., 4.], false);
+    let _y = array_to_val(vec![-10., 20.54553, 30.], false);
+    let optimizer = Optimizer::new(nn.parameters(), 0.1, 1);
+
+    for _ in 0..100 {
+        let y = nn.forward(&x);
+        let mut loss = val(0., false);
+        for j in 0..y.len() {
+            let diff = sub(&y[j], &_y[j]);
+            loss = add(&loss, &pow(&diff, 2.));
+        }
+        loss.backward();
+        optimizer.step();
+        optimizer.zero_grad();
+    }
+
+    let y = nn
+        .forward(&x)
+        .iter()
+        .map(|x| x.output())
+        .collect::<Vec<f32>>();
+
+    println!("y = {:?}", &y);
+}
+
+fn test_computational_graph() {
+    let a = val(1., true);
+    let b = val(2., true);
 
     let learning_rate = 0.01;
 
-    for i in 0..1000 {
-        println!("i = {}", i);
+    for i in 0..10 {
+        let c = pow(&a, 2.);
+        let d = pow(&b, 2.);
+        let e = add(&c, &d);
+        let f = tanh(&e);
+        let o = f.output();
 
-        let cache = &mut HashMap::new();
-        let o = forward(&e, cache);
-        println!("output = {}", o);
-        println!("cache = {:?}", cache);
+        e.backward();
+        //println!("gradients = {:?}", gradients);
 
-        let gradients = &mut HashMap::new();
-        backward(&e, cache, gradients, 1.);
-        println!("gradients = {:?}", gradients);
-
-        if let Some(NodeOp::Value(ref value)) = a.op {
-            let mut value_ref = value.borrow_mut();
-            *value_ref -= learning_rate * gradients.get(&a.id).unwrap();
-            println!("a = {}", *value_ref);
-        }
-
-        if let Some(NodeOp::Value(ref value)) = b.op {
-            let mut value_ref = value.borrow_mut();
-            *value_ref -= learning_rate * gradients.get(&b.id).unwrap();
-            println!("b = {}", *value_ref);
-        }
+        a.step(learning_rate, 1);
+        b.step(learning_rate, 1);
+        a.zero_grad();
+        b.zero_grad();
     }
+    println!("a = {}", a.output());
+    println!("b = {}", b.output());
 }
